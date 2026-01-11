eec5b489e1b611df584d9dd58a13980029ef1791
kidsdz <kidsdz.shop@gmail.com>
2026-01-11 20:10:22 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,31 +1,10 @@
-function getBaladiyatByWilayaId(wilayaId) {
-  return DATA.baladiyat.filter(
-    b => b.wilayaId === wilayaId
-  );
-}
+document.addEventListener("DOMContentLoaded", () => {
+  const wilayaSelect = document.getElementById("wilaya");
 
-const wilayaSelect = document.getElementById("wilaya");
-const baladiyaSelect = document.getElementById("baladiya");
-
-// fill wilayas
-DATA.wilayas.forEach(w => {
-  const option = document.createElement("option");
-  option.value = w.id;
-  option.textContent = `${w.id} - ${w.name}`;
-  wilayaSelect.appendChild(option);
-});
-
-// on change
-wilayaSelect.addEventListener("change", () => {
-  baladiyaSelect.innerHTML =
-    '<option value="">-- اختر البلدية --</option>';
-
-  const wilayaId = Number(wilayaSelect.value);
-  if (!wilayaId) return;
-
-  getBaladiyatByWilayaId(wilayaId).forEach(b => {
+  WILAYAS.forEach(w => {
     const option = document.createElement("option");
-    option.textContent = b.name;
-    baladiyaSelect.appendChild(option);
+    option.value = w.id;
+    option.textContent = `${w.id} - ${w.ar_name}`;
+    wilayaSelect.appendChild(option);
   });
 });
