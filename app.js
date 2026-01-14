10ead4a3684756a5c3e73ce0b6f34a56df501cfa
kidsdz <kidsdz.shop@gmail.com>
2026-01-14 20:46:10 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var GOOGLE_SHEET_URL = "https://script.google.com/macros/s/XXXXXXXX/exec";
+var GOOGLE_SHEET_URL = "https://script.google.com/macros/s/AKfycbzs8ltoWx8gLkn8SKHEfg4xMYQWYoB8fL93nESYlf4cD8CDv2UsUHbDy7mIpzz7kvtquA/exec";
 document.addEventListener("DOMContentLoaded", function () {
 
   /* ===============================
